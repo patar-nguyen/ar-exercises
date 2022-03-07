8ebaae6c861ff754c97bf1553db43ff43184f5e7
Patrick Nguyen <patar.nguyen@gmail.com>
2022-03-07 18:15:22 -0500

    Implement exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Pearl", last_name: "Whale", hourly_rate: 150)
+@store1.employees.create(first_name: "Sandy", last_name: "Cheeks", hourly_rate: 180)
+@store2.employees.create(first_name: "Squidward", last_name: "Tentacles", hourly_rate: 130)
+@store2.employees.create(first_name: "Spongebob", last_name: "Squarepants", hourly_rate: 120)
+@store2.employees.create(first_name: "Patrick", last_name: "Star", hourly_rate: 150)
