c16cb1af4b4ac821f0a31cde452992f7af514cdb
Patrick Nguyen <patar.nguyen@gmail.com>
2022-03-07 19:22:18 -0500

    Implement exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,22 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Enter store name: "
+@user_name = $stdin.gets.chomp
+puts "Enter store salary: "
+@user_revenue = $stdin.gets.chomp
+store = Store.create(name: @user_name, annual_revenue: @user_revenue)
+
+puts store.errors.full_messages
+
+
+puts "Enter first name: "
+@first_name = $stdin.gets.chomp
+puts "Enter last name: "
+@last_name = $stdin.gets.chomp
+puts "Enter hourly rate: "
+@hourly_rate = $stdin.gets.chomp
+employee = Employee.create(first_name: @first_name, last_name: @last_name, hourly_rate: @hourly_rate)
+
+
+puts employee.errors.full_messages
